2a83ff0ec6258416a944b9c74bc290068108ec49
fatima-meh <142571552+fatima-meh@users.noreply.github.com>
2023-08-18 14:19:18 +0500

    Update application.py

--- application.py
+++ application.py
@@ -9,4 +9,4 @@ def register():
 	return render_template('index.html', username = username)
 
 if __name__ == '__main__':
-    application.run(host="0.0.0.0", port="5000")
+    application.run(port=5000)
